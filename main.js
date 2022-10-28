8f252996886375ea0b5b96a5a219ef89a970f5c5
ThompsonKijaka <levandung.it03@gmail.com>
2022-10-28 13:06:34 +0700

    Update click notification box

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function start() {
     const menu = $(".header__navigation--menu");
     const navBlock = $(".header__navigation--list");
     const nav = $(".header__navigation");
+    const homeDownBtn = $(".home__button .down-btn");
+    const downNotifBtn = $(".home__button .home__button--guide");
 
     menu.onclick = function toggleshowNavigation() {
         navBlock.classList.toggle("showNavigation");
@@ -17,6 +19,10 @@ function start() {
     window.onscroll = function (e) {
         const high = (70 - Math.floor(window.scrollY) / 10) + "px";
         nav.style.height = high;
+    }
+
+    homeDownBtn.onclick = function () {
+        downNotifBtn.style.opacity = "0";
     }
 }
 start();
